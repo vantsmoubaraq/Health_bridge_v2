92d7771f90d91046a9f8d07122325fb0f377f890
Ian Kitembe <ianedwin@outlook.com>
2023-07-11 13:39:21 +0300

    completed service modeule (thx renish)

--- web_flask/static/scripts/edit_service.js
+++ web_flask/static/scripts/edit_service.js
@@ -17,11 +17,11 @@ body: JSON.stringify(data),
 headers: {'Content-Type': 'application/json'}
 };
 
-const url = 'http://127.0.0.1:5001/api/v1/service/' + id;
+const url = 'http://127.0.0.1:5001/api/v1/services/' + id;
 
 fetch( url, submit_data
 ).then(response => response.json()).then(
 data => {console.log(data);
-window.location.href = `/single_service/${id}`;}
+window.location.href = `/services`;}
 ).catch(error => console.error(error))
 });
